ae6fd41387af5b2e624f11c7bc1ef755e91e845d
jason7337 <ulloajason10@gmail.com>
2023-07-11 15:37:05 -0600

    mejoras

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const menuToggle = document.querySelector(".menuToggle");
+const sidebar = document.querySelector(".sidebar");
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
+  sidebar.classList.toggle("active");
 });
